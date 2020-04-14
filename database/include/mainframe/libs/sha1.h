f998dfca14003604744d22fe827088e6418af7d1
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2020-04-14 13:38:21 +0200

    moved code from h to cpp
    Change-Id: Ib2fb4443a8ab179d62a9f5e50f9f4994d28569d1

--- database/include/mainframe/libs/sha1.h
+++ database/include/mainframe/libs/sha1.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 #include <inttypes.h>
 
 namespace mainframe {
@@ -8,15 +9,8 @@ namespace mainframe {
 		using Basetype = uint32_t;
 		using Hash = std::vector<Basetype>;
 
-		template<typename T>
-		Hash sha1(const T& input) {
-			Hash h(5);
-			h[0] = 0x67452301, h[1] = 0xEFCDAB89,
-				h[2] = 0x98BADCFE, h[3] = 0x10325476,
-				h[4] = 0xC3D2E1F0;
-			Hash raw = sha1(input.data(), input.size() * sizeof(*input.begin()), h);
-			return raw;
-		}
+		Hash sha1(const std::string& input);
+		Hash sha1(const std::vector<unsigned char>& input);
 
 		Hash sha1(const void* pMem, Basetype iLen, Hash& my_hash);
 		Hash sha1(const void* mem, int len);
