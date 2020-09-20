d183bf775bbbcef8d5b79a2139d2b2bd6e4f0d64
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2020-09-20 18:12:29 +0200

    replace spaces with tabs

--- utils/include/mainframe/utils/string.h
+++ utils/include/mainframe/utils/string.h
@@ -10,23 +10,23 @@ namespace mainframe {
 			std::vector<std::string> split(const std::string& haystack, const std::string& needle);
 			std::string replace(const std::string& haystack, const std::string& needle, const std::string& replacement);
 
-            // https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
-            // trim from start (in place)
-            static void ltrim(std::string& s);
+			// https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+			// trim from start (in place)
+			static void ltrim(std::string& s);
 
-            // trim from end (in place)
-            static void rtrim(std::string& s);
+			// trim from end (in place)
+			static void rtrim(std::string& s);
 
-            // trim from both ends (in place)
-            static void trim(std::string& s);
-            // trim from start (copying)
-            static std::string ltrim_copy(std::string s);
+			// trim from both ends (in place)
+			static void trim(std::string& s);
+			// trim from start (copying)
+			static std::string ltrim_copy(std::string s);
 
-            // trim from end (copying)
-            static std::string rtrim_copy(std::string s);
+			// trim from end (copying)
+			static std::string rtrim_copy(std::string s);
 
-            // trim from both ends (copying)
-            static std::string trim_copy(std::string s);
+			// trim from both ends (copying)
+			static std::string trim_copy(std::string s);
 		};
 	}
 }
