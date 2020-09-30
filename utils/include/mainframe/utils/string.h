b566ebc7cc239afa8fac1b77332bd7016d068deb
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2020-09-30 11:02:25 +0200

    remove static from non class trim functions

--- utils/include/mainframe/utils/string.h
+++ utils/include/mainframe/utils/string.h
@@ -12,21 +12,21 @@ namespace mainframe {
 
 			// https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 			// trim from start (in place)
-			static void ltrim(std::string& s);
+			void ltrim(std::string& s);
 
 			// trim from end (in place)
-			static void rtrim(std::string& s);
+			void rtrim(std::string& s);
 
 			// trim from both ends (in place)
-			static void trim(std::string& s);
+			void trim(std::string& s);
 			// trim from start (copying)
-			static std::string ltrim_copy(std::string s);
+			std::string ltrim_copy(std::string s);
 
 			// trim from end (copying)
-			static std::string rtrim_copy(std::string s);
+			std::string rtrim_copy(std::string s);
 
 			// trim from both ends (copying)
-			static std::string trim_copy(std::string s);
+			std::string trim_copy(std::string s);
 		};
 	}
 }
