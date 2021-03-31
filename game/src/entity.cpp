af3e779690aed1b33c881c19510e779ee6cd4b24
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2021-03-31 17:49:07 +0200

    game:: first step of moving away from shared pointers to unique

--- game/src/entity.cpp
+++ game/src/entity.cpp
@@ -3,7 +3,7 @@
 
 namespace mainframe {
 	namespace game {
-		Entity::Entity(World& world_) : world(world_) {
+		Entity::Entity(World* world_) : world(world_) {
 
 		}
 
@@ -12,11 +12,25 @@ namespace mainframe {
 		}
 
 		void Entity::remove() {
-			world.removeEntity(this);
+			world->removeEntity(this);
 		}
 
 		size_t Entity::getId() const {
 			return id;
+		}
+
+		void Entity::setId(size_t id_) {
+			id = id_;
+		}
+
+		void Entity::generateUniqueId() {
+			static size_t _id = 0;
+
+			id = ++_id;
+		}
+
+		mainframe::math::Matrix& Entity::getMatrix() {
+			return matrix;
 		}
 	}
 }
