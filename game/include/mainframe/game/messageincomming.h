44f19a47cd6d252c429db70bc0ce8660eb60494e
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2021-03-31 17:47:20 +0200

    game: add networking message base classes

--- game/include/mainframe/game/messageincomming.h
+++ game/include/mainframe/game/messageincomming.h
@@ -1,16 +1,57 @@
 #pragma once
 
+#include <mainframe/game/world.h>
+#include <mainframe/game/server.h>
 #include <mainframe/networking/socket.h>
 #include <mainframe/networking/packet.h>
 
 namespace mainframe {
 	namespace game {
-		class Player;
+		class MessageIncomming {
+			mainframe::networking::Packet packet;
+			World* world = nullptr;
+			Server* server = nullptr;
+			Player* player = nullptr;
 
-		class MessageIncomming : public networking::Packet {
 		public:
-			virtual void parse();
-			virtual void process(Player* player);
+			void setBuffer(std::vector<uint8_t> buff);
+			mainframe::networking::Packet& getReader();
+
+			template<class T>
+			T* getWorld() {
+				return dynamic_cast<T*>(world);
+			}
+
+			template<class T>
+			T* getServer() {
+				return dynamic_cast<T*>(server);
+			}
+
+			template<class T>
+			T* getPlayer() {
+				return dynamic_cast<T*>(player);
+			}
+
+			template<class T>
+			void setServer(T* serv) {
+				server = dynamic_cast<Server*>(serv);
+				if (serv != nullptr && server == nullptr) throw std::runtime_error("server dynamic cast failed");
+			}
+
+			template<class T>
+			void setWorld(T* world_) {
+				world = dynamic_cast<World*>(world_);
+				if (world_ != nullptr && world == nullptr) throw std::runtime_error("world dynamic cast failed");
+			}
+
+			template<class T>
+			void setPlayer(T* ply) {
+				player = dynamic_cast<Player*>(ply);
+				if (ply != nullptr && player == nullptr) throw std::runtime_error("player dynamic cast failed");
+			}
+
+			virtual bool read() = 0;
+			virtual void execute() = 0;
 		};
 	}
 }
