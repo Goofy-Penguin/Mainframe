bc6bfe3450f2bedf23473606ba7c6a2fc88451df
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2021-03-31 17:46:33 +0200

    game: clear screenbuffer on new frame

--- game/src/window.cpp
+++ game/src/window.cpp
@@ -187,6 +187,9 @@ namespace mainframe {
 
 		void Window::swapBuffer() const {
 			glfwSwapBuffers(GLFWHANDLE);
+
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		}
 
 		void Window::setSize(const math::Vector2i& size) {
