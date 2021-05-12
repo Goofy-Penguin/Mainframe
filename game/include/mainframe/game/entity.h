b01d67f030f5a649ed72bf7e9ea07e6b235cd20a
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2021-05-12 14:15:02 +0200

    remove unneeded namespaces

--- game/include/mainframe/game/entity.h
+++ game/include/mainframe/game/entity.h
@@ -25,17 +25,18 @@ namespace mainframe {
 			void setId(EntityIdType id_);
 			EntityIdType getId() const;
 
-			mainframe::math::Matrix& getMatrix();
-			const mainframe::math::Matrix& getMatrix() const;
-			virtual mainframe::math::Vector3 getPosition() const;
-			virtual mainframe::math::Vector3 getScale() const;
-			virtual mainframe::math::Vector3 getRotation() const;
-			virtual mainframe::math::Vector3 getVelocity() const;
+			math::Matrix& getMatrix();
+			const math::Matrix& getMatrix() const;
 
-			virtual void setVelocity(const mainframe::math::Vector3& force);
-			virtual void setPosition(const mainframe::math::Vector3& pos);
-			virtual void setScale(const mainframe::math::Vector3& scale);
-			virtual void setRotation(const mainframe::math::Vector3& rot);
+			virtual math::Vector3 getPosition() const;
+			virtual math::Vector3 getScale() const;
+			virtual math::Vector3 getRotation() const;
+			virtual math::Vector3 getVelocity() const;
+
+			virtual void setVelocity(const math::Vector3& force);
+			virtual void setPosition(const math::Vector3& pos);
+			virtual void setScale(const math::Vector3& scale);
+			virtual void setRotation(const math::Vector3& rot);
 
 			virtual void initialize();
 			virtual void update();
