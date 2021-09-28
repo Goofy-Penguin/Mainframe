2a9f0f7c9b28be5bc0c90716d53560f668db0db6
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2021-09-28 19:59:24 +0200

    whitespace

--- render/include/mainframe/render/font.h
+++ render/include/mainframe/render/font.h
@@ -26,11 +26,11 @@ namespace mainframe {
 
 			Font& operator=(const Font& other) = delete;
 			Font& operator=(Font&& other) = delete;
-			
+
 			void Release();
 
 			~Font();
-			
+
 			// distance between two lines of text that are under eachother
 			float getLineHeight() const;
 
@@ -42,11 +42,11 @@ namespace mainframe {
 			float getKerning(const Glyph& left, const Glyph& right) const;
 
 			Glyph loadGlyph(FT_ULong character);
-			
+
 			const Glyph& getGlyph(uint32_t codepoint) const;
-			
+
 			math::Vector2 getStringSize(const std::string& text) const;
-			
+
 			static std::string toUTF8(const std::wstring text);
 		};
 	}
