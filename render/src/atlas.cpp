ed65e642a50f62ced9aaa772b49e72617e8bef2b
failcake <failcake.work@gmail.com>
2021-09-28 22:41:59 +0200

    Change GL_REPEAT to GL_CLAMP_TO_EDGE

--- render/src/atlas.cpp
+++ render/src/atlas.cpp
@@ -11,9 +11,8 @@ namespace mainframe {
 			glBindTexture(GL_TEXTURE_2D, glTexture);
 
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size, size, 0, GL_RGBA, GL_FLOAT, nullptr);
-
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		}
