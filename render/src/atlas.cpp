5d28e0d42c356229363a444a1db0f298bf7f54cf
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2021-09-28 19:54:07 +0200

    fix black texture

--- render/src/atlas.cpp
+++ render/src/atlas.cpp
@@ -9,7 +9,13 @@ namespace mainframe {
 		Atlas::Atlas() {
 			glGenTextures(1, &glTexture);
 			glBindTexture(GL_TEXTURE_2D, glTexture);
+
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size, size, 0, GL_RGBA, GL_FLOAT, nullptr);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		}
 
 		Atlas::~Atlas() {
