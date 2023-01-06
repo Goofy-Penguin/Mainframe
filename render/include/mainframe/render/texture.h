63e179951d882c25ae39dad349f78915ea13067e
FailCake <edunad@users.noreply.github.com>
2023-01-06 10:11:54 +0100

    - Replace lodepng with stb_image - Add support to bind to a different shader texture

--- render/include/mainframe/render/texture.h
+++ render/include/mainframe/render/texture.h
@@ -2,76 +2,75 @@
 
 #include <mainframe/math/vector2.h>
 #include <mainframe/render/color.h>
+
 #include <memory>
 #include <vector>
 #include <string>
 #include <climits>
 
-namespace mainframe {
-	namespace render {
-		class TextureHandle {
-		public:
-			unsigned int glHandle = UINT_MAX;
+namespace mainframe::render {
+	class TextureHandle {
+	public:
+		unsigned int glHandle = UINT_MAX;
 
-			void reset();
+		void reset();
 
-			TextureHandle() = default;
-			~TextureHandle();
-		};
+		TextureHandle() = default;
+		~TextureHandle();
+	};
 
-		class Texture {
-		private:
-			unsigned int quality = 0x2600; // GL_NEAREST
-			unsigned int wrap = 0x2901; // GL_REPEAT
+	class Texture {
+	private:
+		unsigned int quality = 0x2600; // GL_NEAREST
+		unsigned int wrap = 0x2901; // GL_REPEAT
 
-			mainframe::math::Vector2i originalSize;
-			mainframe::math::Vector2i size;
-			std::vector<Color> pixels;
-			std::shared_ptr<TextureHandle> handle = std::make_shared<TextureHandle>();
+		mainframe::math::Vector2i originalSize;
+		mainframe::math::Vector2i size;
+		std::vector<Color> pixels;
+		std::shared_ptr<TextureHandle> handle = std::make_shared<TextureHandle>();
 
-			void checkPixels() const;
+		void checkPixels() const;
 
-		public:
-			unsigned int blend_src = 0x0303; // GL_SRC_ALPHA
-			unsigned int blend_dest = 0x0302; // GL_ONE_MINUS_SRC_ALPHA
+	public:
+		unsigned int blend_src = 0x0303; // GL_SRC_ALPHA
+		unsigned int blend_dest = 0x0302; // GL_ONE_MINUS_SRC_ALPHA
 
-			Texture() = default;
-			Texture(const Texture& t) = default;
-			Texture(const mainframe::math::Vector2i& initsize, const mainframe::render::Color& bgcol = Colors::Transparent);
-			Texture(const std::string& fileName);
+		Texture() = default;
+		Texture(const Texture& t) = default;
+		Texture(const mainframe::math::Vector2i& initsize, const mainframe::render::Color& bgcol = Colors::Transparent);
+		Texture(const std::string& fileName);
 
-			const mainframe::math::Vector2i& getSize() const;
-			const mainframe::math::Vector2i& getOriginalSize() const;
-			void resize(const mainframe::math::Vector2i& newsize);
+		const mainframe::math::Vector2i& getSize() const;
+		const mainframe::math::Vector2i& getOriginalSize() const;
+		void resize(const mainframe::math::Vector2i& newsize);
 
-			mainframe::render::Color& getPixel(unsigned x, unsigned y);
-			const mainframe::render::Color& getPixel(unsigned x, unsigned y) const;
-			mainframe::render::Color& getPixel(const mainframe::math::Vector2i& pos);
-			const mainframe::render::Color& getPixel(const mainframe::math::Vector2i& pos) const;
+		mainframe::render::Color& getPixel(unsigned x, unsigned y);
+		const mainframe::render::Color& getPixel(unsigned x, unsigned y) const;
+		mainframe::render::Color& getPixel(const mainframe::math::Vector2i& pos);
+		const mainframe::render::Color& getPixel(const mainframe::math::Vector2i& pos) const;
 
-			void setPixel(const mainframe::math::Vector2i& pos, const mainframe::render::Color& col);
-			void setPixels(const mainframe::math::Vector2i& size_, const std::vector<Color>& data);
-			void setMaxAnisotropy();
+		void setPixel(const mainframe::math::Vector2i& pos, const mainframe::render::Color& col);
+		void setPixels(const mainframe::math::Vector2i& size_, const std::vector<Color>& data);
+		void setMaxAnisotropy();
 
-			void bind();
-			void upload();
-			void setQuality(unsigned int quality);
-			void setWrap(unsigned int wrap);
-			void setBlend(unsigned int blend_src, unsigned int blend_dest);
+		void bind(int index = 0);
+		void upload();
+		void setQuality(unsigned int quality);
+		void setWrap(unsigned int wrap);
+		void setBlend(unsigned int blend_src, unsigned int blend_dest);
 
-			void use();
-			void use() const;
-			void clear(const mainframe::render::Color& bgcol = Colors::Transparent);
-			void unloadPixels();
+		void use();
+		void use() const;
+		void clear(const mainframe::render::Color& bgcol = Colors::Transparent);
+		void unloadPixels();
 
-			std::vector<uint8_t> toRGBABytes();
-			void fromRGBABytes(std::vector<uint8_t> buff);
+		std::vector<uint8_t> toRGBABytes();
+		void fromRGBABytes(std::vector<uint8_t> buff);
 
-			std::vector<Color>& data();
-			const std::vector<Color>& data() const;
-			unsigned int getHandle() const;
-			std::shared_ptr<TextureHandle>& getSharedHandle();
-			void setSharedHandle(std::shared_ptr<TextureHandle>& handle_);
-		};
-	}
+		std::vector<Color>& data();
+		const std::vector<Color>& data() const;
+		unsigned int getHandle() const;
+		std::shared_ptr<TextureHandle>& getSharedHandle();
+		void setSharedHandle(std::shared_ptr<TextureHandle>& handle_);
+	};
 }
